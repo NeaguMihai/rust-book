aa773ecd43699e218a31bc987115105e4f082211
Mihai <mihain199961@gmail.com>
2022-12-04 20:40:36 +0200

    finished book

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,34 @@
+use std::{
+    fs,
+    io::{BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream}, ops::Add,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buff_reader = BufReader::new(&stream);
+    let req_line = buff_reader.lines().next().unwrap().unwrap();
+    let mut response = String::from("");
+    if req_line == "GET / HTTP/1.1" {
+
+        let status_line = "HTTP/1.1 200 OK";
+        let content = fs::read_to_string("src/templates/index.html").unwrap();
+        response = response.add(&format!("{status_line}\r\n\r\n{content}"));
+    } else {
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        response = response.add(&format!("{status_line}"));    
+    }
+    stream
+    .write_all(
+        response.as_bytes(),
+    )
+    .unwrap();
 }
